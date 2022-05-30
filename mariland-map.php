d17429b7808270a479b9f32f96e800d7b073c3a9
Dhan Ortiz <dhan.marlan.ortiz@gmail.com>
2022-05-30 21:21:10 +0800

    updated map

--- mariland-map.php
+++ mariland-map.php
@@ -2,7 +2,7 @@
             <div class="container">
                 <h2 class="mb-5 text-center">Mariland</h2>
                 <div class="img-thumbnail animate__animated animate__fadeIn">
-                    <img src="images/mariland-map.png" class="img-fluid" alt="">
+                    <img src="images/mariland-map.jpg" class="img-fluid" alt="">
                 </div>
             </div>
         </section>
